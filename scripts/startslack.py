20614b21ccff2b6b22896f9c0a1661bdb85ab64b
Pedro Correa <pedro8correa@gmail.com>
2018-07-05 12:11:37 -0300

    Removing slack api key exposed

--- scripts/startslack.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-os.system('cd ~/ && cd terminal-slack && export SLACK_TOKEN="xoxp-230817812470-229609364256-392253317154-5e15373d3daa936d1573948e0851b3ce" && node main.js')
-
