612fd7a1e7e5ad1988eb44e92028ee980c5a4ff3
Pedro Correa <pedro8correa@gmail.com>
2019-11-02 23:00:57 -0300

    Changed Xresources theme

--- X/symlink.sh
+++ X/symlink.sh
@@ -5,24 +5,24 @@ DIR=$(pwd)
 function linkDotfile {
   DEST="${HOME}/.config/${1}"
 
-  if [ -h ~/$DEST ]; then
+  if [ -h "$DEST" ]; then
     # Existing symlink
-    echo "Removing existing symlink: ${DEST}"
+    echo "Removing existing symlink: $DEST"
     rm ${DEST}
 
-  elif [ -f "${DEST}" ]; then
+  elif [ -f "$DEST" ]; then
     # Existing file
-    echo "Backing up existing file: ${DEST}"
+    echo "Backing up existing file: $DEST"
     mv ${DEST}{,.${dateStr}}
 
-  elif [ -d "${DEST}" ]; then
+  elif [ -d "$DEST" ]; then
     # Existing dir
-    echo "Backing up existing die: ${DEST}"
+    echo "Backing up existing die: $DEST"
     mv ${DEST}{,.${dateStr}}
   fi
 
-  echo "Creating new symlink: ${DEST}"
-  ln -s "${DIR}/${1}" ${DEST}
+  echo "Creating new symlink: $DEST"
+  ln -s "${DIR}/${1}" $DEST
 }
 
 for colorscheme in $(ls X/Xres.*); do
