54153da274512f830d9adf09443ffddd14dea87b
Pedro Correa <pedro8correa@gmail.com>
2019-11-02 22:44:23 -0300

    Added Xresources themes and script to auto symlink

--- Makefile
+++ Makefile
@@ -4,9 +4,11 @@ SHELL := /bin/bash
 MAKEFLAGS += --silent
 
 install:
-		chmod +x scripts/symlink.sh
 		bash scripts/symlink.sh
-		chmod +x scripts/symlinkConfigFile.sh
 		bash scripts/symlinkConfigFile.sh
 		echo "Setup is done, you can use normally"
+
+Xresources:
+		bash X/symlink.sh
+		echo "Symlink X themes completed"
 
---
+++ X/symlink.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIR=$(pwd)
+
+function linkDotfile {
+  DEST="${HOME}/.config/${1}"
+
+  if [ -h ~/$DEST ]; then
+    # Existing symlink
+    echo "Removing existing symlink: ${DEST}"
+    rm ${DEST}
+
+  elif [ -f "${DEST}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${DEST}"
+    mv ${DEST}{,.${dateStr}}
+
+  elif [ -d "${DEST}" ]; then
+    # Existing dir
+    echo "Backing up existing die: ${DEST}"
+    mv ${DEST}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${DEST}"
+  ln -s "${DIR}/${1}" ${DEST}
+}
+
+for colorscheme in $(ls X/Xres.*); do
+  linkDotfile $colorscheme
+done
